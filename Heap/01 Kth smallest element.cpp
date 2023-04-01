96ff8f0ca5bc19615e9aa75fcc71edf060acebff
Aashi Gupta <97161658+Aashi779@users.noreply.github.com>
2023-04-01 23:33:07 +0530

    Update 01 Kth smallest element.cpp

--- Heap/01 Kth smallest element.cpp
+++ Heap/01 Kth smallest element.cpp
@@ -3,7 +3,7 @@ public:
     int findKthSmallest(vector<int>& nums, int k) {
         priority_queue<int> maxh;
         
-        for(int i = 0; i< maxh.size(); i++){
+        for(int i = 0; i< nums.size(); i++){
            maxh.push(nums[i]);
            if(maxh.size()>k){
                maxh.pop();
