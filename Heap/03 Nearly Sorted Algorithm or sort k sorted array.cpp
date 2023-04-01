c9e2dcfb763fbaadae7e5896a6a1b758ff2b685f
Aashi Gupta <97161658+Aashi779@users.noreply.github.com>
2023-04-01 17:20:46 +0530

    Update 03 Nearly Sorted Algorithm or sort k sorted array.cpp

--- Heap/03 Nearly Sorted Algorithm or sort k sorted array.cpp
+++ Heap/03 Nearly Sorted Algorithm or sort k sorted array.cpp
@@ -1,38 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-  int t;
-	int n,k;
-  cin>>t;
-	while (t--)
-  {
-    cin>>n>>k;
-	    int a;
+/*
+Create a Min Heap of size K+1 with the first K+1 elements. We are creating a heap of size K as 
+the element can be at most K distance from its index in a sorted array. One by one remove the 
+min element from the heap, put it in the result array, and add a new element to the heap from 
+the remaining elements.
+*/
+
+// range[i-k, i+k]
+vector<int> nearlysorted(){    
     vector<int> v;
-	    priority_queue<int,vector<int>,greater<int>> minh;
-    for (int i=0;i<n;i++)
-	    {
-      cin>>a;
-      minh.push(a);
-      if (minh.size()>k)
-      {
-        int val= minh.top();
-	      minh.pop();
-        v.push_back(val);
+    priority_queue<int,vector<int>,greater<int>> minh;
+    for (int i=0 ;i<n ;i++){
+      minh.push(nums[i]);
+      if (minh.size()>k){
+        int val = minh.top();
+	v.push_back(val);
+	minh.pop();
       }
-    }
-    while (!minh.empty())
-    {
-      int val = minh.top();
-      minh.pop();
-      v.push_back(val);
-    }
-    for (int i=0;i<n;i++){
-      cout<<v[i]<<" ";
-    }
-    cout<<"\n";
-  }
-  return 0;
+ }
+ while (!minh.empty()){
+    int val = minh.top();
+    v.push_back(val);
+    minh.pop();
+ }
+    return v;   
 }
-    
